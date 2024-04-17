ba7be017a6e7496bd22a973f46806d9e2991f9fc
Caio <caiowhitmore@outlook.com>
2024-04-17 12:43:19 +0100

    first test complete for ticket 3

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -51,3 +51,40 @@ describe("GET /api", () => {
     });
   });
 });
+
+describe("GET /api/articles/:article_id", () => {
+  it("should return 200 and be equal to article 1", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.article).toEqual([
+          {
+            article_id: 1,
+            title: "Living in the shadow of a great man",
+            topic: "mitch",
+            author: "butter_bridge",
+            body: "I find this existence challenging",
+            created_at: "2020-07-09T20:11:00.000Z",
+            votes: 100,
+            article_img_url:
+              "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          },
+        ]);
+      });
+  });
+
+  // Check for the specific values expected for the requested article where applicable.
+  // E.g. Given a request to GET /api/articles/1, for article_id on the response we would expect the value of 1 rather than .any(Number).
+  //  Status 400: Bad request, invalid article_id
+  // it("if given a bad request should return with status 400", () => {
+  //   return request(app).get("/api/articles/notNumber").expect(400);
+  // });
+
+  // Responds with error object including status and message.
+  // E.g. GET /api/articles/notanumber
+  //  Status 404: Not found, article_id does not exist
+
+  // Responds with error object including status and message.
+  // E.g. GET /api/articles/9999
+});
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const { getTopics } = require("./controllers/topics-controller");
+const { getTopics } = require("./controllers/topics-controller.js");
+const { getArticle } = require("./controllers/articles-controller.js");
 const endPointsJson = require("./endpoints.json");
 
 app.get("/api/topics", getTopics);
@@ -8,6 +9,8 @@ app.get("/api/topics", getTopics);
 app.get("/api", (req, res, next) => {
   res.status(200).send(endPointsJson);
 });
+
+app.get("/api/articles/:article_id", getArticle);
 
 // 404 path not found
 app.use((req, res, next) => {
