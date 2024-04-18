91eee88bf0d27ef19bdd9f86b57f9c88549005fd
Caio <caiowhitmore@outlook.com>
2024-04-18 12:23:36 +0100

    test to be reveiwed

--- models/articles-model.js
+++ models/articles-model.js
@@ -1,9 +1,29 @@
+const db = require("../db/connection");
+
 exports.fetchArticle = (article_id) => {
-  const db = require("../db/connection");
+  console.log("we are model");
   return db
     .query(`SELECT * FROM articles WHERE article_id=$1;`, [article_id])
     .then(({ rows }) => {
       const article = rows[0];
-      return article;
+      if (!article) {
+        console.log("we are error");
+        return Promise.reject({
+          status: 404,
+          msg: `No user found for article id: ${article_id}`,
+        });
+      } else {
+        return article;
+      }
+    });
+};
+
+exports.fetchAllArticles = () => {
+  return db
+    .query(
+      `SELECT title, topic, author, created_at, article_img_url, votes FROM articles ORDER BY created_at DESC ;`
+    )
+    .then((result) => {
+      return result.rows;
     });
 };
