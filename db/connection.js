89d82a60da0458f526c2c9231eb2709649db12fc
Caio <caiowhitmore@outlook.com>
2024-05-01 11:10:03 +0100

    hosting

--- db/connection.js
+++ db/connection.js
@@ -4,10 +4,16 @@ const ENV = process.env.NODE_ENV || "development";
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
-// /home/caio/northcoders/backend/be-nc-news/.env.news_database
 
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
