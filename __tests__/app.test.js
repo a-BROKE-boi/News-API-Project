eb2e5e05a2561abe1aba6679efae8810c1b78f5b
Caio <caiowhitmore@outlook.com>
2024-05-09 14:28:19 +0100

    updated endpoints json

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -92,7 +92,7 @@ describe("GET /api/articles/:article_id", () => {
   });
 });
 
-describe.only("GET /api/articles", () => {
+describe("GET /api/articles", () => {
   it("should return an array of article objects and if its the expected shape and length", () => {
     return request(app)
       .get("/api/articles")
